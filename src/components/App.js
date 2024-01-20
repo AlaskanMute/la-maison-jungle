d4994670b6d69f54857ed12eb42aae5b935f33b7
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-20 16:16:36 +0100

    add useState to Cart.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 // App.js
 import logo from '../assets/logo.png'
 import Banner from './Banner';
-//import Cart from './Cart'
+import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 import QuestionForm from './QuestionForm';
 import Footer from './Footer';
@@ -13,7 +13,7 @@ function App() {
                         <img src={logo} alt='La maison jungle' className='lmj-logo' />
 				                <h1 className='lmj-title'>La maison jungle</h1>
                 </Banner>
-                {/*<Cart/> */}
+                <Cart/> 
                 <ShoppingList/>
                 <QuestionForm/>
                 <Footer/>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,22 +1,23 @@
 // Cart.js
 
 import '../styles/Cart.css'
+import { useState } from 'react'
 
 
 function Cart() {
 	const monsteraPrice = 8
-	const ivyPrice = 10
-	const flowerPrice = 15
+	const [cart, updateCart] = useState(0)
+
 	return (
 		<div className='lmj-cart'>
-			<h2>Panier</h2>
-			<ul>
-				<li>Monstera : {monsteraPrice}€</li>
-				<li>Lierre : {ivyPrice}€</li>
-				<li>Fleurs : {flowerPrice}€</li>
-			</ul>
-			Total des achat : {monsteraPrice + ivyPrice + flowerPrice}€
-		</div>
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+                </button>
+            </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+        </div>
 	)
 }
 
